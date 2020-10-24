944c6431a0b2ccdbabe55f6d234d64ef185bcdc2
tanya-ui <70968949+tanya-ui@users.noreply.github.com>
2020-10-24 22:29:58 +0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int mas[5] = { 1, 2, 3, 4, 5 };
+	int* begin = mas;
+	int* end = mas + sizeof(mas) / sizeof(*mas) - 1;
+
+	while (begin < end)
+	{
+		*begin ^= *end ^= *begin ^= *end;
+		begin++;
+		end--;
+	}
+	for (int i = 0; i < sizeof(mas) / sizeof(*mas); i++)
+	{
+		cout << *(mas + 1) << " ";
+	}
+}
